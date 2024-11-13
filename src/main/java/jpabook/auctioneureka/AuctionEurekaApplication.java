9f3b86394baa3040847d5d3d9e1bc3a8ad33e4f7
areum0116 <areum0116@naver.com>
2024-11-13 16:54:21 +0900

    init

---
+++ src/main/java/jpabook/auctioneureka/AuctionEurekaApplication.java
@@ -0,0 +1,13 @@
+package jpabook.auctioneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionEurekaApplication.class, args);
+    }
+
+}
